ab7c25eaeb0b6e829cc58272ca575ed3b2619cf8
Luphuc2005 <phucga150625@gmail.com>
2025-09-30 19:55:41 +0700

    CẬP NHẬT

---
+++ Student-Management-System_CSharp_SGU2025/DTO/DEKHONGBIMATFOLDER.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_System_CSharp_SGU2025.BUS
+{
+    internal class DEKHONGBIMATFOLDER
+    {
+    }
+}
