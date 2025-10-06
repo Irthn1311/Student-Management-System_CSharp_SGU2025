601e826275d1fb5bafdec107100863ffdd7db7aa
Irthn1311 <nhuutri1311@gmail.com>
2025-10-07 05:50:19 +0700

    feat: thiêt kế giao diện sidebar, header

---
+++ Student-Management-System_CSharp_SGU2025/GUI/Controls/ucHeader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management_System_CSharp_SGU2025.GUI
+{
+    public partial class ucHeader : UserControl
+    {
+        public ucHeader()
+        {
+            InitializeComponent();
+        }
+
+        // Public methods để update header text
+        public void UpdateHeader(string title, string breadcrumb)
+        {
+            lblTitle.Text = title;
+            lblBreadcrumb.Text = breadcrumb;
+        }
+
+        public void SetTitle(string title)
+        {
+            lblTitle.Text = title;
+        }
+
+        public void SetBreadcrumb(string breadcrumb)
+        {
+            lblBreadcrumb.Text = breadcrumb;
+        }
+    }
+}
---
+++ Student-Management-System_CSharp_SGU2025/GUI/Controls/ucSidebar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management_System_CSharp_SGU2025.GUI
+{
+    public partial class ucSidebar : UserControl
+    {
+        public ucSidebar()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlHeader_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flpnlNav_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlLogout_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnTaiKhoan_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
