488ce49e256ebc88a57833bacde1098079fc41a8
Luphuc2005 <phucga150625@gmail.com>
2025-10-07 16:25:48 +0700

    feat: tạo statcardKhoi để tái sử dụng

---
+++ Student-Management-System_CSharp_SGU2025/GUI/statcardLHP/StatCardKhoi.cs
@@ -0,0 +1,28 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Student_Management_System_CSharp_SGU2025.GUI.statcardLHP
+{
+    public partial class StatCardKhoi : UserControl
+    {
+        public StatCardKhoi()
+        {
+            InitializeComponent();
+        }
+
+        // Gán dữ liệu và màu nền cho thẻ
+        public void SetData(string tenKhoi, string soLop, string soHocSinh)
+        {
+            lblTenKhoi.Text = tenKhoi;
+            lblSoLop.Text = soLop;
+            lblSoHocSinh.Text = soHocSinh;
+           
+
+
+
+        }
+
+    }
+}
---
+++ Student-Management-System_CSharp_SGU2025/GUI/statcardLHP/StatCardPhanCongGiangDay.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Student_Management_System_CSharp_SGU2025.GUI.statcardLHP
+{
+    partial class StatCardPhanCongGiangDay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTen = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblSoLuong = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.SuspendLayout();
+            // 
+            // lblTen
+            // 
+            this.lblTen.BackColor = System.Drawing.Color.Transparent;
+            this.lblTen.Font = new System.Drawing.Font("Segoe UI", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTen.Location = new System.Drawing.Point(68, 15);
+            this.lblTen.Name = "lblTen";
+            this.lblTen.Size = new System.Drawing.Size(79, 27);
+            this.lblTen.TabIndex = 0;
+            this.lblTen.Text = "Tên Card";
+            // 
+            // lblSoLuong
+            // 
+            this.lblSoLuong.BackColor = System.Drawing.Color.Transparent;
+            this.lblSoLuong.Font = new System.Drawing.Font("Segoe UI", 14.26415F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoLuong.Location = new System.Drawing.Point(54, 48);
+            this.lblSoLuong.Name = "lblSoLuong";
+            this.lblSoLuong.Size = new System.Drawing.Size(93, 32);
+            this.lblSoLuong.TabIndex = 1;
+            this.lblSoLuong.Text = "Số lượng";
+            this.lblSoLuong.Click += new System.EventHandler(this.lblSoLuong_Click);
+            // 
+            // StatCardPhanCongGiangDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblSoLuong);
+            this.Controls.Add(this.lblTen);
+            this.Name = "StatCardPhanCongGiangDay";
+            this.Size = new System.Drawing.Size(211, 99);
+            this.Load += new System.EventHandler(this.StatCardPhanCongGiangDay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblTen;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblSoLuong;
+    }
+}
