64dde81497ff201e07cd7eaf7f8817b3abed721a
lambao72 <lbao7262@gmail.com>
2025-10-17 15:46:00 +0700

    tạo các lớp DTO cho Giáo Viên Hạnh kiểm thời khóa biểu

---
+++ Student-Management-System_CSharp_SGU2025/DTO/HanhKiemDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_System_CSharp_SGU2025.DTO
+{
+ class HanhKiemDTO
+    {
+        public string MaHocSinh { get; set; }
+        public int MaHocKy { get; set; }
+        public string XepLoai { get; set; } // Ánh xạ trường XepLoai (NVARCHAR)
+        public string NhanXet { get; set; } // Ánh xạ trường NhanXet (TEXT)
+
+        // Thuộc tính bổ sung/hiển thị (Dữ liệu từ JOIN)
+        public string HoTenHocSinh { get; set; }
+        public string TenHocKy { get; set; }
+    }
+}
